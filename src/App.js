712228b300d81d087d81d9136e5400cfdaa38999
ratchagarn <ratchagarn@gmail.com>
2020-09-25 18:21:35 +0700

    Update footer

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React, { useState } from 'react'
 import styled from 'styled-components'
 import { Layout, Row, Col } from 'antd'
 
+import pkg from '../package.json'
+
 import CodeEditor from './components/CodeEditor'
 import Preview from './components/Preview'
 
 const { Header, Footer, Content } = Layout
+
+console.log(pkg)
 
 const defaultCode = `
 graph TD
@@ -38,7 +42,16 @@ function App() {
           </Col>
         </Row>
       </Content>
-      <Footer>Make with Love by @ratchagarn</Footer>
+      <Footer>
+        <Row type="flex">
+          <Col span={14}>Make with Love by @ratchagarn</Col>
+          <Col span={10}>
+            <Row type="flex" justify="end">
+              <Col>v{pkg.version}</Col>
+            </Row>
+          </Col>
+        </Row>
+      </Footer>
     </Layout>
   )
 
