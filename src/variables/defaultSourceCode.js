50768ded4aa4fb80b7d8eea7d978b1b9fbee33a3
ratchagarn <ratchagarn@gmail.com>
2020-09-28 11:05:13 +0700

    Update example source code

--- src/variables/defaultSourceCode.js
+++ src/variables/defaultSourceCode.js
@@ -1,8 +1,13 @@
 export default `
-graph TD
-  A[Christmas] -->|Get money| B(Go shopping)
-  B --> C{Let me think}
-  C -->|One| D[Laptop]
-  C -->|Two| E[iPhone]
-  C -->|Three| F[fa:fa-car Car]
+sequenceDiagram
+    participant Alice
+    participant Bob
+    Alice->>John: Hello John, how are you?
+    loop Healthcheck
+        John->>John: Fight against hypochondria
+    end
+    Note right of John: Rational thoughts <br/>prevail!
+    John-->>Alice: Great!
+    John->>Bob: How about you?
+    Bob-->>John: Jolly good!
 `.trim()
